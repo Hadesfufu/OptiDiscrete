42d83ddcf68adf0af4f393f19e1f4f54550b8842
Fiona Reziga <fiona.reziga@gmail.com>
2018-06-17 23:28:30 +0200

    Menu Algo génétique

--- src/Main.java
+++ src/Main.java
@@ -27,11 +27,70 @@ public class Main {
                 a.taboo(2000, 30);
                 break;
             case 2:
-                a.algoGenetique(10, 2000);
+                int populationSize = geneticPopulationSizeChoice();
+                int tries = geneticTriesChoice();
+
+                System.out.println("\n----Lancement de l'algorithme génétique");
+                a.algoGenetique(populationSize, tries);
                 break;
             default:
                 break;
         }
+    }
+
+    private static int geneticPopulationSizeChoice() {
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nEntrez la taille de la population : ");
+
+        selection = input.nextInt();
+        return selection;
+    }
+
+    private static int geneticTriesChoice() {
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nEntrez le nombre d'itérations : ");
+
+        selection = input.nextInt();
+        return selection;
+    }
+
+    private static int tabooMethodChoice() {
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nChoix de la méthode de génération des voisins");
+        System.out.println("-------------------------");
+        System.out.println("1 - Move : Echange deux clients de deux routes différentes");
+        System.out.println("2 - Swap : Déplacement d'un client dans une route aléatoire");
+        System.out.println("3 - Move et Swap");
+
+
+        selection = input.nextInt();
+        return selection;
+    }
+
+    private static int tabooTriesChoice() {
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nEntrez le nombre d'essais : ");
+
+        selection = input.nextInt();
+        return selection;
+    }
+
+    private static int tabooSizeChoice() {
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nEntrez la taille de la liste : ");
+
+        selection = input.nextInt();
+        return selection;
     }
 
     private static int generationBaseSolutionChoice() {
@@ -52,9 +111,8 @@ public class Main {
         int selection;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Projet d'optimisation discrète");
-        System.out.println("-------------------------\n");
-        System.out.println("0 - Quitter");
+        System.out.println("\nChoix de la métaheuristique");
+        System.out.println("-------------------------");
         System.out.println("1 - Taboo");
         System.out.println("2 - Algorithme génétique");
 
